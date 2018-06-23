d2807b34ea6005c13e3fc0afb951048b9320e8d8
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    curtana-remake: Build seperate system & vendor HIDL libs

--- device.mk
+++ device.mk
@@ -63,6 +63,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
