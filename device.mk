9a30edf2b76a16b6de471dde12772cc50915f706
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-10-19 16:59:43 +0200

    curtana: Build NoCutoutOverlay
    Change-Id: I45edeb3ae20f374cd4510bad83b3a51059a7e743

--- device.mk
+++ device.mk
@@ -13,11 +13,34 @@ $(call inherit-product-if-exists, vendor/xiaomi/curtana/curtana-vendor.mk)
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
+=======
+# Inherit common stuffs
+$(call inherit-product, vendor/xiaomi/curtana/curtana-vendor.mk)
+>>>>>>> curtana: Build NoCutoutOverlay
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Audio
+PRODUCT_COPY_FILES += \
+$(LOCAL_PATH)/audio/audio_platform_info.xml
+$(LOCAL_PATH)/audio/audio_policy_volumes.xml
+$(LOCAL_PATH)/audio/default_volume_tables.xml
+$(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd937x.xml
+$(LOCAL_PATH)/audio/sound_trigger_platform_info.xml
+
+# Treble
+PRODUCT_USE_VNDK_OVERRIDE := true
