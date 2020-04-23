6b2ddad0223544d616581eec61541f971224fa8a
XtremeQ(MArshadR) <mohamedarshadr@gmail.com>
2020-04-23 23:43:57 +0800

    curtana:initial commit

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 #
 # Copyright (C) 2020 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
 
 set -e
 
 # Required!
+<<<<<<< HEAD
 export DEVICE=curtana
 export DEVICE_COMMON=sm6250-common
 export VENDOR=xiaomi
-
 export DEVICE_BRINGUP_YEAR=2020
-
-"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
