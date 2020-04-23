53921d3d39bed7d26b246700cca0de2f1620a0cc
SahilSonar <sss.sonar2003@gmail.com>
2020-04-24 06:35:13 +0800

    Init BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #
 # Copyright (C) 2020 The LineageOS Project
 #
@@ -20,3 +21,25 @@ TARGET_SCREEN_DENSITY := 440
 
 # Kernel
 TARGET_KERNEL_CONFIG := curtana_defconfig
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a76  
+
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2020-02-01
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+
+-include vendor/xiaomi/curtana/BoardConfigVendor.mk
