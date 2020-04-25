4f2e039a1f1966dfbb1ff46b28e6e886222b0e47
MArshadR <mohamedarshadr@gmail.com>
2020-04-26 03:07:46 +0800

    added target kernel arch & stuffs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,8 @@ endif
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
+TARGET_KERNEL_ARCH := arm64
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1
 BOARD_KERNEL_CMDLINE := androidboot.selinux.permissive
 TARGET_KERNEL_SOURCE := kernel/xiaomi/curtana
 TARGET_KERNEL_CONFIG := curtana_defconfig
