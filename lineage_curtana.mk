8baa2b447ae0648662d18d2a645061b6de3f308c
MArshadR <mohamedarshadr@gmail.com>
2020-04-29 08:46:47 +0000

    corrected device name

--- lineage_curtana.mk
+++ lineage_curtana.mk
@@ -5,6 +5,10 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from curtana
 $(call inherit-product, device/xiaomi/curtana/device.mk)
 
 # Inherit some LineageOS stuff.
@@ -16,8 +20,8 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_curtana
 PRODUCT_DEVICE := curtana
-PRODUCT_BRAND := Redmi/Xiaomi
-PRODUCT_MODEL := Redmi Note 9 Pro
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 9 Pro Max
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
