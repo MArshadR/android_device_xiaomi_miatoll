eb016899b54909a500f016184409a3fcc8b37fee
MArshadR <mohamedarshadr@gmail.com>
2020-04-29 15:11:54 +0000

    Clean-ify Android.mk

--- Android.mk
+++ Android.mk
@@ -16,6 +16,9 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),curtana)
-subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
-endif
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+
