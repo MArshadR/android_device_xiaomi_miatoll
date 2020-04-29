c90b8d78fa5b16c241a004bfa6866e1ba507cd61
MArshadR <mohamedarshadr@gmail.com>
2020-04-29 21:50:30 +0000

    Removed audio policies as it's causing issues

--- device.mk
+++ device.mk
@@ -2,9 +2,10 @@
 # Copyright (C) 2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
-#
 
-# Inherit common stuffs 
+LOCAL_PATH := device/xiaomi/curtana
+
+# Inherit common stuffs
 $(call inherit-product, vendor/xiaomi/curtana/curtana-vendor.mk)
 
 # Overlays
@@ -22,14 +23,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-# Audio
-PRODUCT_COPY_FILES += \
-$(LOCAL_PATH)/audio/audio_platform_info.xml
-$(LOCAL_PATH)/audio/audio_policy_volumes.xml
-$(LOCAL_PATH)/audio/default_volume_tables.xml
-$(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd937x.xml
-$(LOCAL_PATH)/audio/sound_trigger_platform_info.xml
 
 # Treble
 PRODUCT_USE_VNDK_OVERRIDE := true
