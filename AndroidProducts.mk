6047c650401aea3618cd5fefa5fd068251a82ea3
MArshadR <mohamedarshadr@gmail.com>
2020-04-29 07:40:29 +0000

    corrected paths to makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,3 @@
-  PRODUCT_MAKEFILES := \
-     lineage_curtana.mk
-
-COOMON_LUNCH_CHOICES := \
-     lineage_curtana-userdebug \
-     lineage_curtana-user \
-     lineage_curtana-eng
+PRODUCT_MAKEFILES := \
+ $(DEVICE_PATH)/lineage_curtana.mk
 
