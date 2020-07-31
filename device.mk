a31f265860f9ff628e4492046b58e084e3c6846a
MArshadR <mohamedarshadr@gmail.com>
2020-07-31 12:18:42 +0800

    treewide: Unimport Audio stuffs for reimport

--- device.mk
+++ device.mk
@@ -18,9 +18,6 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     Snap
 
-# Inherit common stuffs
-$(call inherit-product, vendor/xiaomi/curtana/curtana-vendor.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
@@ -36,14 +33,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-# Audio
-PRODUCT_COPY_FILES += \
-$(LOCAL_PATH)/audio/audio_platform_info.xml
-$(LOCAL_PATH)/audio/audio_policy_volumes.xml
-$(LOCAL_PATH)/audio/default_volume_tables.xml
-$(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd937x.xml
-$(LOCAL_PATH)/audio/sound_trigger_platform_info.xml
 
 # Treble
 PRODUCT_USE_VNDK_OVERRIDE := true
