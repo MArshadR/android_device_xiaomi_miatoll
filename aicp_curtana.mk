b0c59091a76468bb042081d0d75142815c10f5c3
MArshadR <mohamedarshadr@gmail.com>
2020-07-31 12:03:31 +0800

    curtana: Update build fingerprint from CURTANAGlobal_V11.0.10.0.QJWMIXM

--- aicp_curtana.mk
+++ aicp_curtana.mk
@@ -20,6 +20,6 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 9s
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Redmi/curtana_global/curtana:10/QKQ1.191215.002/V11.0.9.0.QJWMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Redmi/curtana_global/curtana:10/QKQ1.191215.002/V11.0.10.0.QJWMIXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
