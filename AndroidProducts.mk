8f78fd8b79a056845c4296a4797b03729ba867e0
MArshadR <mohamedarshadr@gmail.com>
2020-07-31 09:17:21 +0800

    curtana: AICP Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,18 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-<<<<<<< HEAD
-    $(LOCAL_DIR)/lineage_curtana.mk
+ $(LOCAL_PATH)/aicp_curtana.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_curtana-user \
-    lineage_curtana-userdebug \
-    lineage_curtana-eng
-=======
- $(LOCAL_PATH)/havoc_curtana.mk
-
-COMMON_LUNCH_CHOICES := \
- havoc_curtana-userdebug \
- havoc_curtana-user \
- havoc_curtana-eng
->>>>>>> curtana: bringup for Havoc-OS
+ aicp_curtana-userdebug \
+ aicp_curtana-user \
+ aicp_curtana-eng
