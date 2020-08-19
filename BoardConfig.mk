b4ca058a8626f9805ee7a23b1f1b3e8e56ca477b
MArshadR <mohamedarshadr@gmail.com>
2020-08-19 22:10:36 +0800

    curtana: Set Kernel stuffs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,3 +16,16 @@ TARGET_2ND_CPU_VARIANT := cortex-a9
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x01000000
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := cust_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
+BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 earlycon=msm_geni_serial,0xa88000 loop.max_part=7 cgroup.memory=nokmem,nosocket msm_rtb.enabled=1
+
