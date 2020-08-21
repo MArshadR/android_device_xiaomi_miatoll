08687ad21ccbbab6e7e81eb3e826138607c83821
Dhina17 <dhinalogu@gmail.com>
2020-08-21 19:46:51 +0800

    curtana: Import keylayouts from stock

--- device.mk
+++ device.mk
@@ -59,6 +59,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=392
 
+# Keylayout
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
