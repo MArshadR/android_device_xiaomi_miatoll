50c23beb2ac75df53e4d9a235f241ae71ed86c66
MArshadR <mohamedarshadr@gmail.com>
2020-09-16 02:34:51 +0800

    Set PRODUCT_COPY_FILES - Missed that

--- device.mk
+++ device.mk
@@ -15,8 +15,8 @@
 #
 
 # Vendors
-$(call inherit-product, vendor/xiaomi/sm6250-common/sm6250-common.mk)
-$(call inherit-product, vendor/xiaomi/miatoll/miatoll-common.mk)
+$(call inherit-product, vendor/xiaomi/sm6250-common/sm6250-common-vendor.mk)
+$(call inherit-product, vendor/xiaomi/miatoll/miatoll-vendor.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -76,6 +76,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=392
 
 # Keylayout
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
