00be5f4cf8e34be7c5938475b59ead024319f093
MArshadR <mohamedarshadr@gmail.com>
2020-09-16 02:33:27 +0800

    Properly set source config path

--- lineage_curtana.mk
+++ lineage_curtana.mk
@@ -2,14 +2,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit common configs from AOSPA
-$(call inherit-product, vendor/pa/config/common_full_phone.mk)
+# Inherit common configs from LineageOS
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from curtana
 $(call inherit-product, device/xiaomi/curtana/device.mk)
 
 # Device identifier
-PRODUCT_NAME := pa_curtana
+PRODUCT_NAME := lineage_curtana
 PRODUCT_DEVICE := curtana
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 9s
